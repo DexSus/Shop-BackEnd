cfd37e6e6a4386384c0746cb3c4185e1d0b400ab
DexSus <insomniadex@gmail.com>
2024-02-24 11:29:13 +0200

    install morgan and catch errors in requests

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const userRoutes = require('./api/routes/users.js');
 const productRoutes = require('./api/routes/products.js');
@@ -9,13 +10,28 @@ const orderRoutes = require('./api/routes/orders.js');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({ "users":["userOne", "userTwo", "userThree"] });
-});
-
 app.use(bodyParser.json());
+
+app.use(morgan('dev'))
+
+// Routes which should handle the request
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({ 
+        error: {
+            message: error.message
+        }
+    })
+})
 
 app.listen(5000, () => { console.log("Server started on 5000 port"); });
